3d32085ad973596c3720678b90324fb280d6469d
Fredy <fcbastidas20@gmail.com>
2025-06-12 21:36:20 -0500

    Tarea Semana 02 - Programación Tradicional vs POO

---
+++ Parcial 01/Semana 03/3.1 Tarea Semana 03 Programacion Tradicional.py
@@ -0,0 +1,22 @@
+# Registro de temperaturas - Programación Tradicional
+
+
+# Función para ingresar las temperaturas de 7 días
+def ingresar_temperaturas():
+    temperaturas = []
+    for dia in range(7):
+        temperatura = float(input(f"Ingrese la temperatura del día {dia + 1}: "))
+        temperaturas.append(temperatura)
+    return temperaturas
+
+# Función para calcular el promedio semanal
+def calcular_promedio(temperaturas):
+    total = sum(temperaturas)
+    promedio = total / len(temperaturas)
+    return promedio
+
+# Mostramos en consola
+print("REGISTRO SEMANAL DE TEMPERATURAS")
+temperaturas = ingresar_temperaturas()
+promedio = calcular_promedio(temperaturas)
+print(f"El promedio semanal es: {promedio:.2f}°C")
---
+++ Parcial 01/Semana 03/3.2 Tarea semana 03 POO.py
@@ -0,0 +1,43 @@
+# Registro de temperaturas - POO
+
+
+# Creamos una clase que representa el clima semanal
+class Clima:
+    def __init__(self):
+        # Encapsulamos la lista de temperaturas
+        self.__temperaturas = []
+
+    # Método para ingresar datos
+    def ingresar_temperaturas(self):
+        for dia in range(7):
+            temperatura = float(input(f"Ingrese la temperatura del día {dia + 1}: "))
+            self.__temperaturas.append(temperatura)
+
+    # Método para calcular promedio
+    def calcular_promedio(self):
+        if len(self.__temperaturas) == 7:
+            return sum(self.__temperaturas) / 7
+        else:
+            return 0
+
+# Clase hija que hereda de Clima y añade una descripción
+class ClimaConDescripcion(Clima):
+    def describir_clima(self):
+        promedio = self.calcular_promedio()
+        if promedio > 30:
+            return "Muy caluroso"
+        elif promedio >= 20:
+            return "Agradable"
+        else:
+            return "Frío"
+
+# Mostramos en cosola
+print("REGISTRO SEMANAL DE TEMPERATURAS (POO)")
+clima = ClimaConDescripcion()  # Creamos un objeto
+
+clima.ingresar_temperaturas() # Llamamos método
+promedio = clima.calcular_promedio()
+descripcion = clima.describir_clima()
+
+print(f"Promedio: {promedio:.2f}°C")
+print(f"Descripción del clima: {descripcion}")
